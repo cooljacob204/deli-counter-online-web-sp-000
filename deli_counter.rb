3c1b4944b7164daa99d3e7f1b5c4c550e381d2ae
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-17 15:23:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,7 @@
 # Write your code here.
 
 def line(line)
-  if (line.size == 0) puts "The Line is currently empty." end
+  if (line.size == 0) 
+    puts "The Line is currently empty." 
+  end
 end
