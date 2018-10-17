f3fea664273ba61bc3e7ab2f8085c82ea0c5d02e
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-17 15:31:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(line)
   output = ""
   output << "The line is currently empty." if (line.size == 0)
   if (line.size > 0)
-    print "The line is currently:"
-    line.each_with_index { |value, index| print " #{index + 1}. #{value}."}
+    output << "The line is currently:"
+    line.each_with_index { |value, index| output << " #{index + 1}. #{value}."}
   end
 end
